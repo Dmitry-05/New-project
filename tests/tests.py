a7c6d7d4b59cb1feaaf29dc8f7003c960fcec6ef
Dmitry-05 <valovdmitry5@mail.ru>
2021-05-18 20:27:51 +0500

    added tests folder and gitignore file

---
+++ tests/tests.py
@@ -0,0 +1,17 @@
+import unittest
+import sys, os
+
+sys.path.append(os.getcwd())
+from exercises_python import *
+
+class arithmetic_test(unittest.TestCase):
+    def test_arithmetic_returns_answers(self):
+        self.assertEqual(arithmetic(2, 3, "+"), 5)
+        self.assertEqual(arithmetic(5, 3, "-"), 2)
+        self.assertEqual(arithmetic(5, 4, "*"), 20)
+        self.assertEqual(arithmetic(6, 3, "/"), 2)
+        self.assertEqual(arithmetic(2, 3, ""), "Unknown operation")
+
+
+if __name__ == '__main__':
+    unittest.main()
