7818be29bac800c6325c7061785cc71c8345fda6
Dmitry-05 <valovdmitry5@mail.ru>
2021-05-18 20:40:53 +0500

    Second ex done

--- exercises_python.py
+++ exercises_python.py
@@ -3,14 +3,26 @@
 # аргумент +, сложить их; если —, то вычесть; * — умножить; / — разделить
 # (первое на второе). В остальных случаях вернуть строку "Неизвестная операция"
 
-def arithmetic(num1, num2, oper):
-    if oper == "+":
-        return num1 + num2
-    elif oper == "-":
-        return num1 - num2
-    elif oper == "*":
-        return num1 * num2
-    elif oper == "/":
-        return num1 / num2
+# def arithmetic(num1, num2, oper):
+#     if oper == "+":
+#         return num1 + num2
+#     elif oper == "-":
+#         return num1 - num2
+#     elif oper == "*":
+#         return num1 * num2
+#     elif oper == "/":
+#         return num1 / num2
+#     else:
+#         return "Unknown operation"
+
+# 2 Написать функцию is_year_leap, принимающую 1 аргумент — год,
+# и возвращающую True, если год високосный, и False иначе.
+
+def is_year_leap(year):
+    if year % 4 == 0:
+        return True
     else:
-        return "Unknown operation"
+        return False
+
+print(is_year_leap(2000))
+print(is_year_leap(2001))
